e2b3f169c8754ab4e1dbbab7abe438c3bee53f65
Razvan Statescu <email@example.com>
2022-07-05 15:46:36 +0300

    init

---
+++ config/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const parts = require('./webpack.parts')
+const { mode, analyze } = require('webpack-nano/argv')
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+const common = merge([
+  { output: { path: path.resolve(process.cwd(), 'dist') } },
+  parts.page({ title: 'Sh*t Kanye says' }),
+  parts.loadSvg(),
+  parts.svelte(mode),
+  parts.extractCSS({ loaders: [parts.postcss()] }),
+  parts.cleanDist(),
+  parts.useWebpackBar(),
+  parts.useDotenv()
+])
+
+const development = merge([
+  { entry: ['./src/index.ts', 'webpack-plugin-serve/client'] },
+  { target: 'web' },
+  {plugins: [new NodePolyfillPlugin()]},
+  parts.generateSourceMaps({ type: 'eval-source-map' }),
+  parts.esbuild(),
+  parts.devServer()
+])
+
+const production = merge(
+  [
+    { entry: ['./src/index.ts'] },
+    {plugins: [new NodePolyfillPlugin()]},
+    parts.typescript(),
+    parts.optimize(),
+    analyze && parts.analyze()
+  ].filter(Boolean)
+)
+
+const getConfig = mode => {
+  switch (mode) {
+    case 'production':
+      return merge(common, production, { mode })
+    case 'development':
+      return merge(common, development, { mode })
+    default:
+      throw new Error(`Unknown mode, ${mode}`)
+  }
+}
+
+module.exports = getConfig(mode)
---
+++ src/App.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import { Web3Auth } from '@web3auth/web3auth'
+  import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from '@web3auth/base'
+  import { onMount } from 'svelte'
+
+  let clientId = process.env.CLIENT_ID
+  let web3Auth
+  let provider
+  let userInfo
+
+  const subscribeAuthEvents = web3auth => {
+    web3auth.on('connected', data => {
+      if (data) {
+        getUserInfo()
+      }
+    })
+  }
+
+  onMount(() => {
+    const init = async () => {
+      try {
+        web3Auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: CHAIN_NAMESPACES.EIP155
+          }
+        })
+
+        subscribeAuthEvents(web3Auth)
+
+        await web3Auth.initModal()
+
+        provider = web3Auth.provider
+      } catch (error) {
+        console.error('err', error)
+      }
+    }
+
+    init()
+  })
+
+  const login = async () => {
+    if (!web3Auth) {
+      console.log('web3auth not initialized yet')
+      return
+    }
+    provider = await web3Auth.connect()
+  }
+
+  const logout = async () => {
+    if (!web3Auth) {
+      console.log('web3auth not initialized yet')
+      return
+    }
+    await web3Auth.logout()
+    provider = null
+  }
+
+  const getUserInfo = async () => {
+    if (!web3Auth) {
+      console.log('web3auth not initialized yet')
+      return
+    }
+    const user = await web3Auth.getUserInfo()
+    userInfo = user ? user.email : ''
+  }
+
+  const continueToApp = async () => {
+    web3Auth.provider
+      .request({
+        method: 'eth_private_key'
+      })
+      .then(privKey => {
+        const userDataLocalStorage = JSON.stringify({
+          ...localStorage,
+          privateKey: privKey
+        })
+        const base64UserData = btoa(userDataLocalStorage)
+        switch (new URL(window?.location.toString()).searchParams.get('platform')) {
+          case 'electron': {
+            window.location.assign(`circles-something://open?user_data=${base64UserData}`)
+            break
+          }
+          case 'capacitor': {
+            window.location.assign(
+              `https://dummy-circles-wallet.vercel.app/open/?user_data=${base64UserData}`
+            )
+            break
+          }
+          default: {
+            window.location.assign(`http://localhost:3000?user_data=${base64UserData}`)
+            break
+          }
+        }
+      })
+  }
+</script>
+
+<div style="margin-top: 100px;">
+  <p>{userInfo}</p>
+  {#if !provider}
+    <button on:click={login}>Login</button>
+  {:else}
+    <button on:click={logout}>Logout</button>
+  {/if}
+  <button on:click={getUserInfo}>User Info</button>
+
+  {#if userInfo}
+    <div class="mt-8"><button on:click={continueToApp}>Continue to App</button></div>
+  {/if}
+</div>
